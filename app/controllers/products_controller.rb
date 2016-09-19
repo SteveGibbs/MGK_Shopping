38dd9239ff4731b91ce0f5b5ca2566abd1a002cf
skal9606 <skal9606@gmail.com>
2016-09-19 12:35:53 +1000

    Samit: Product CRUD system created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,16 +1,40 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
   end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
-  def new
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to products_path
   end
 
   def show
+    @product = Product.find(params[:id])
+
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:product_name, :product_desc1, :product_desc2, :price, :active, :image, :brand_id)
   end
 end
