f346243ad4f1497696f7cdc85d71d9adcd9d571d
SteveGibbs <gibbsstephen@hotmail.com>
2016-09-20 22:01:47 +1000

    for ajax have now managed to display nested array to match ruby display which we will remove, for example an order from shopping cart may have more than one item so need to run nested for loop to find each product bought

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -6,13 +6,15 @@
    // An array of objects
 
    for (var i = 0; i < data.length; i++) {
+
+     for (var j = 0; j <data[i].items.length; j++){
      console.log( data[i] );
      var name = data[i].name;
      var address1 = data[i].address1;
      var status = data[i].status;
-     var productName = data[i].items[0].product.product_name;
-     var price = data[i].items[0].product.price;
-     var quantity = data[i].items[0].quantity;
+     var productName = data[i].items[j].product.product_name;
+     var price = data[i].items[j].product.price;
+     var quantity = data[i].items[j].quantity;
      var total = quantity * price;  //hack for display -  may select multiple items - to be revisited
      var $li = $("<li>").text("Name: " + name);
      var $li2 = $("<li>").text("Address: " + address1);
@@ -22,25 +24,36 @@
      var $li6 = $("<li>").text("Quantity: "+ quantity);
      var $li7 = $("<li>").text("Total $" + total);
      var $li8 = $("<br>").text("");
-
-      $("body").append( $li ).append($li2).append($li3).append($li4).append($li5).append($li6).append($li7).append($li8);
+      // document.body.innerText = "";
+      $(".clear").append( $li ).append($li2).append($li3).append($li4).append($li5).append($li6).append($li7).append($li8);
     //  $("body").append( $li2 );
+
+    // $(".clear").innerHTML = "";
      console.log( name );
      console.log(address1);
      console.log(status);
   }
-
+}
  };
+
 
  var baseURL = "/orders";
  var getOrderDisplay = function(){
+
    $.ajax({
      url: baseURL,
      type: "GET",
      dataType: "JSON"
    }).done(orderDisplay);
+
+
  };
 
+
+
  $(document).on("turbolinks:load", function(){
+
    $("#order_display").on("click", getOrderDisplay);
+
+
  });
