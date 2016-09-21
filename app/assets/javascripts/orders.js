8605f7d41648bb5ffd6e619a2202afae9973d4f2
SteveGibbs <gibbsstephen@hotmail.com>
2016-09-21 20:41:00 +1000

    reconfiguring all orders page to provide a better display of orders with live polling

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -5,41 +5,30 @@
    // Data is visible here: http://localhost:3000/orders.json
    // An array of objects
     $(".clear_display").empty();
+
+
+
    for (var i = 0; i < data.length; i++) {
      var name = data[i].name;
      var address1 = data[i].address1;
      var status = data[i].status;
+     var $li8 = $("<br>").text("");
      var $li = $("<li>").text("Name: " + name);
      var $li2 = $("<li>").text("Address: " + address1);
      var $li3 = $("<li>").text("Status: " + status);
-     $(".clear_display").append( $li ).append($li2).append($li3);
+     $(".clear_display").append($li8).append( $li ).append($li2).append($li3);
      for (var j = 0; j <data[i].items.length; j++){
      console.log( data[i] );
-    //  var name = data[i].name;
-    //  var address1 = data[i].address1;
-    //  var status = data[i].status;
+
      var productName = data[i].items[j].product.product_name;
      var price = data[i].items[j].product.price;
      var quantity = data[i].items[j].quantity;
+     var orderID = data[i].items[j].order_id;
      var total = quantity * price;  //hack for display -  may select multiple items - to be revisited
-    //  var $li = $("<li>").text("Name: " + name);
-    //  var $li2 = $("<li>").text("Address: " + address1);
-    //  var $li3 = $("<li>").text("Status: " + status);
-     var $li4 = $("<li>").text("Product Name: "+ productName);
-     var $li5 = $("<li>").text("Price:  " + price);
-     var $li6 = $("<li>").text("Quantity: "+ quantity);
-     var $li7 = $("<li>").text("Total $" + total);
-     var $li8 = $("<br>").text("");
-      // document.body.innerText = "";
+     var $li4 = $("<li>").text("Purchased " + quantity + " of " + productName + " items @ price of $ " + price + " for $ " + total + " on order_id " + orderID);
+     
+      $(".clear_display").append($li4);
 
-      $(".clear_display").append($li4).append($li5).append($li6).append($li7).append($li8);
-      // $(".clear").append( $li ).append($li2).append($li3).append($li4).append($li5).append($li6).append($li7).append($li8);
-    //  $("body").append( $li2 );
-
-
-     console.log( name );
-     console.log(address1);
-     console.log(status);
   }
 }
  };
@@ -53,9 +42,9 @@
      type: "GET",
      dataType: "JSON"
    }).done(orderDisplay);
-   window.setInterval(function(){
-     getOrderDisplay();
-   }, 10000);
+  //  window.setInterval(function(){
+  //    getOrderDisplay();
+  //  }, 10000);
 
 
  };
